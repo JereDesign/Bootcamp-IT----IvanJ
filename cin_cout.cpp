3d36ae9eda5b3a633f5716f5dd7bd98a63b2e501
JereDesign <33057840+JereDesign@users.noreply.github.com>
2018-11-27 09:29:27 +0100

    HelloWorld
    Added first file

---
+++ cin_cout.cpp
@@ -0,0 +1,72 @@
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;                   
+
+int main() {
+
+/*
+int a;
+char b;
+
+cout << "Unesi neki broj: ";
+cin >> a;
+
+cout << "Unesi neko slovo: ";
+cin >> b;
+
+cout << "Rezultat" <<endl;
+cout << a << endl;
+cout << b << endl;
+*/
+
+//cout << "Danas" << endl << "je" << endl << "lijep dan" <<endl;
+
+
+/*
+cout << "Ivan Jeremic" << endl;
+
+int a1;
+int a2;
+
+cout << "Unesi prvi broj";
+cin >> a1;
+
+cout << "Unesi drugi broj";
+cin >> a2;
+
+cout << "Rezultat: ";
+cout << a1;
+cout << a2 << endl;
+*/
+
+
+/*
+string ime;
+string prezime;
+
+cout << "Unesite svoje ime: ";
+cin >> ime;
+
+cout << "Unesite svoje prezime: ";
+cin >> prezime;
+
+cout << "Vi mora da ste: " << endl << ime << endl << prezime << endl << "Zadovoljstvo mi je biti vase racunalo!";
+*/
+
+//cout << "Ja \nsam \"Pero\"";
+
+
+cout << "x vrijednost \t y vrijednost \t izraz \t rezultat";
+cout << "\n10 \t\t 5 \t\t x=y+3 \t x=8";
+cout << "\n1000 \t\t 5 \t\t x=y-2 \t x=3 ";
+cout << "\n10 \t\t 5 \t\t x=y*5 \t x=25 ";
+cout << "\n10 \t\t 5 \t\t x=y/x \t x=2 ";
+cout << "\n10 \t\t 5 \t\t x=x%y \t x=0";
+
+cout << "\n\nKrizic kruzic";
+cout << "\n_|_|_\n_|_|_\n | |";
+
+}
---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>                     
+
+using namespace std;                   
+
+
+int a;
+char b;
+
+cin >> a;
+cin >> b;
+
